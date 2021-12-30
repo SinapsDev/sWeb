42f3effb3756a2427d96f18a2841b943aa600625
Rayan <alaouirayan55@gmail.com>
2021-12-30 23:01:02 +0100

    feat(client/landingpage): change route on button click

--- client/src/components/home/index.tsx
+++ client/src/components/home/index.tsx
@@ -1,18 +1,20 @@
 import React from 'react';
+import { useHistory } from "react-router-dom";
 import { Button } from '../../utils/Button/index'
 import Cinematic from "../../assets/cinematic.mp4";
 
 import './index.css';
 
 const Home: React.FC = () => {
+    const history = useHistory();
     return (
         <>
             <div className="main-block">
                 <h1 className='server-title'>Limitless RP</h1>
                 <p className='main-text'>Bienvenue sur Limitless RP, un serveur de RolePlay serieux, ici pous pouvez lire le règlement, consulter le WIKI, postuler pour votre whitelist, gérer vos dossiers et plein d'autres fonctionnalités.</p>
                 <div className="btn-container">
-                    <Button Width='30%' Color='#000' Background='#52be80'>SE CONNECTER</Button>
-                    <Button Width='30%' Color='#000' Background='#52be80'>S'INSCRIRE</Button>
+                    <Button onClick={() => history.push('/login')} Width='30%' Color='#000' Background='#52be80'>SE CONNECTER</Button>
+                    <Button onClick={() => history.push('/register')} Width='30%' Color='#000' Background='#52be80'>S'INSCRIRE</Button>
                 </div>
             </div>
             <div className="black-opacity"></div>
