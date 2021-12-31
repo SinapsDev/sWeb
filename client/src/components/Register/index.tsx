7bc35d4880c29687a2221beed9e68b4240897bc8
Rayan <alaouirayan55@gmail.com>
2021-12-31 21:17:12 +0100

    feat(client/register): send register request to api

--- client/src/components/Register/index.tsx
+++ client/src/components/Register/index.tsx
@@ -1,20 +1,41 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Axios from 'axios';
 import { Button } from '../../utils/Button';
 import './index.css'
 import Cinematic from "../../assets/cinematic.mp4";
 
 const Register: React.FC = () => {
+    const [email, setEmail] = useState('');
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+    const [confirmPassword, setConfirmPassword] = useState('');
+
+    const submitRegister = () => {
+        if (username !== '' && password !== '' && confirmPassword !== '' && email !== '') {
+            Axios.post('http://localhost:3001/register', {
+                email,
+                username,
+                password,
+                confirmPassword
+            }).then(res => {
+                console.log(res.data)
+            }).catch(err => {
+                console.log(err)
+            })
+        }
+    }
+
     return (
         <>
             <div className="main-container">
                 <div className="left-container">
                     <h1 className='register-title'>S'INSCRIRE</h1>
                     <div className="register-form">
-                        <input type="text" name="email" placeholder='Entrez votre email' className='register-input' />
-                        <input type="text" name="username" placeholder='Entrez votre username' className='register-input' />
-                        <input type="password" name="password" placeholder='Entrez votre mot de passe' className='register-input' />
-                        <input type="password" name="confirmpassword" placeholder='Confirmer votre mot de passe' className='register-input' />
-                        <Button Margin='2rem auto' Background='#1c2833' Color='#d5d8dc' Width='100%' Height='60px'>S'INSCRIRE</Button>
+                        <input onChange={(e:any) => setEmail(e.target.value)} type="text" name="email" placeholder='Entrez votre email' className='register-input' />
+                        <input onChange={(e:any) => setUsername(e.target.value)} type="text" name="username" placeholder='Entrez votre username' className='register-input' />
+                        <input onChange={(e:any) => setPassword(e.target.value)} type="password" name="password" placeholder='Entrez votre mot de passe' className='register-input' />
+                        <input onChange={(e:any) => setConfirmPassword(e.target.value)} type="password" name="confirmpassword" placeholder='Confirmer votre mot de passe' className='register-input' />
+                        <Button onClick={() => submitRegister()} Margin='2rem auto' Background='#1c2833' Color='#d5d8dc' Width='100%' Height='60px'>S'INSCRIRE</Button>
                     </div>
                 </div>
                 <div className="right-container">
