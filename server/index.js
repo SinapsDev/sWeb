bb8245b63c051793904883dd5f8299cf035fc4b2
Rayan <alaouirayan55@gmail.com>
2021-12-31 21:45:26 +0100

    feat(server/login): login sessions & cookies

--- server/index.js
+++ server/index.js
@@ -3,11 +3,32 @@ const app = express();
 const mysql = require("mysql");
 const cors = require("cors");
 
+const bodyParser = require("body-parser");
+const cookieParser = require('cookie-parser');
+const session = require('express-session');
+
 const bcrypt = require("bcrypt");
 const saltRounds = 10;
 
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+    origin: ['http://localhost:3000'],
+    methods: ['GET', 'POST'],
+    credentials: true
+}));
+
+app.use(cookieParser());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(session({
+    key: 'userId',
+    secret: 'sWeb', // put a strong password, it's too dangerous 
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+      expires: 60 * 60 * 24
+    }
+}));
 
 const db = mysql.createConnection({
   user: "root",
@@ -47,7 +68,15 @@ app.post('/register', (req, res) => {
   } else {
     res.send("Passwords do not match");
   }
-}); 
+});
+
+app.get('/login', (req, res) => {
+  if (req.session.user) {
+    res.send({loggedIn: true, user: req.session.user});
+  } else {
+    res.send({loggedIn: false});
+  }
+});
 
 app.post('/login', (req, res) => {
   const { username, password } = req.body;
@@ -63,7 +92,9 @@ app.post('/login', (req, res) => {
             console.log(err);
           } else {
             if (response) {
-              res.send("Success");
+              req.session.user = result[0]
+              console.log(req.session.user)
+              res.send(result[0]);
             } else {
               res.send("Incorrect password");
             }
