d648ef987e23f2297999982da347060e9aa63bf3
Rayan <alaouirayan55@gmail.com>
2022-01-01 14:06:46 +0100

    fix(client/navbar): goto login or register on button click

--- client/src/components/navbar/RightNav.tsx
+++ client/src/components/navbar/RightNav.tsx
@@ -14,56 +14,54 @@ type Props = {
 
 function RightNav(props: Props) {
     return (
-        <Router>
-            <S.Ul open={props.open}>
-                <NavLink
-                    to='/menu1'
-                    activeStyle={{
-                        fontWeight: 'bold',
-                        color: '#52be80',
-                    }}
-                >
-                    <S.Li>ACCUEIL</S.Li>
-                </NavLink>
-                <NavLink
-                    to='/menu2'
-                    activeStyle={{
-                        fontWeight: 'bold',
-                        color: '#52be80',
-                    }}
-                >
-                    <S.Li>WIKI</S.Li>
-                </NavLink>
-                <NavLink
-                    to='/menu3'
-                    activeStyle={{
-                        fontWeight: 'bold',
-                        color: '#52be80',
-                    }}
-                >
-                    <S.Li>WHITELIST</S.Li>
-                </NavLink>
-                <NavLink
-                    to='/menu4'
-                    activeStyle={{
-                        fontWeight: 'bold',
-                        color: '#52be80',
-                    }}
-                >
-                    <S.Li>S'IDENTIFIER</S.Li>
-                </NavLink>
-                <NavLink
-                    to='/menu5'
-                    activeStyle={{
-                        fontWeight: 'bold',
-                        color: '#52be80',
-                    }}
-                >
+        <S.Ul open={props.open}>
+            <NavLink
+                to='/menu1'
+                activeStyle={{
+                    fontWeight: 'bold',
+                    color: '#52be80',
+                }}
+            >
+                <S.Li>ACCUEIL</S.Li>
+            </NavLink>
+            <NavLink
+                to='/menu2'
+                activeStyle={{
+                    fontWeight: 'bold',
+                    color: '#52be80',
+                }}
+            >
+                <S.Li>WIKI</S.Li>
+            </NavLink>
+            <NavLink
+                to='/menu3'
+                activeStyle={{
+                    fontWeight: 'bold',
+                    color: '#52be80',
+                }}
+            >
+                <S.Li>WHITELIST</S.Li>
+            </NavLink>
+            <NavLink
+                to='/login'
+                activeStyle={{
+                    fontWeight: 'bold',
+                    color: '#52be80',
+                }}
+            >
+                <S.Li>S'IDENTIFIER</S.Li>
+            </NavLink>
+            <NavLink
+                to='/register'
+                activeStyle={{
+                    fontWeight: 'bold',
+                    color: '#52be80',
+                }}
+            >
 
-                    <S.Li2>S'INSCRIRE</S.Li2>
-                </NavLink>
-            </S.Ul>
-        </Router>
+                <S.Li2>S'INSCRIRE</S.Li2>
+            </NavLink>
+        </S.Ul>
     );
 }
 
